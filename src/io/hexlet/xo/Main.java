4f914b0cf358c2b101a38de607e605226e5e432d
Ivar Mahhonin <ivar.mahhonin@gmail.com>
2016-10-12 20:31:10 +0300

    Added StringBuilder knowledge to the game

--- src/io/hexlet/xo/Main.java
+++ src/io/hexlet/xo/Main.java
@@ -1,106 +1,43 @@
+package io.hexlet.xo;
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Point;
 import io.hexlet.xo.model.Player;
 import io.hexlet.xo.model.Figure;
 import io.hexlet.xo.model.Game;
 import io.hexlet.xo.model.GameBuilder;
-import io.hexlet.xo.model.exceptions.*;
 import io.hexlet.xo.view.*;
-import io.hexlet.xo.controllers.*;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String... args) {
-        final Field field = new Field();
-        final CurrentMoveController cmc = new CurrentMoveController();
-
-
-        if (!cmc.currentMove(field).equals(Figure.X)){
-            throw new RuntimeException("do not returns X when the field is empty");
-        }
-
-        for (int i = 0; i < field.getSize(); i++)
-            for (int i2 = 0; i2 < field.getSize(); i2++) {
-                if (i * i2 == 4) break;
-                final Point p = new Point(i, i2);
-                final Figure figure = (i * 3 + i2) % 2 == 0 ? Figure.X : Figure.O;
-                field.setFigure(p, figure);
-                System.out.printf("putting figure: %s to the: X:%d Y:%d\n", figure, p.getX(), p.getY());
-                if (cmc.currentMove(field).equals(figure)){
-                    throw new RuntimeException(String.format("returns %s for the field", figure));
-                }
-
-            }
-
-        checkField(null, null, null, null, null, null, null, null, null, null);
-        checkField("X", "X", "X", "X", null, null, null, null, null, null);
-        checkField("O", null, null, "O", "O", "O", "O", "X", null, null);
-        checkField("X", null, null, null, null, null, null, "X", "X", "X");
-        checkField("O", "O", "X", null, null, "O", null, null, null, "O");
-
-        testPrivateMethod("countFiguresInTheRow", CurrentMoveController.class, Field.class, Integer.class);
-        testPublicMethod("currentMove", CurrentMoveController.class, Field.class);
-        testPublicMethod("applyFigure", MoveController.class, Field.class, Point.class, Figure.class);
-        testPublicMethod("getWinner", WinnerController.class, Field.class);
-        testPrivateMethod("checkDiag1", WinnerController.class, Field.class);
-        testPrivateMethod("checkDiag2", WinnerController.class, Field.class);
-        testPrivateMethod("checkColumn", WinnerController.class, Field.class, Integer.class);
-        testPrivateMethod("checkRow", WinnerController.class, Field.class, Integer.class);
-
-        final Player p = new Player("Slava", Figure.X);
-        if (!p.getName().equals("Slava")) throw new RuntimeException(String.format("Player.getName returns %s instead of %s", p.getName(), "Slava"));
-        if (!p.getFigure().equals(Figure.X)) throw new RuntimeException(String.format("Player.getFigure returns %s instead of %s", p.getFigure(), Figure.X));
+        final Player p1 = new Player("Slava", Figure.X);
         final Player p2 = new Player("Gleb", Figure.O);
-        final Game g = new GameBuilder().player1(p).player2(p2).field(field).name("XO").build();
-        if (!g.getPlayer1().equals(p)) throw new RuntimeException(String.format("Game.getPlayer1 returns %s instead of %s", g.getPlayer1(), p));
-        if (!g.getPlayer2().equals(p2)) throw new RuntimeException(String.format("Game.getPlayer2 returns %s instead of %s", g.getPlayer2(), p2));
-    }
+        final Field field = new Field();
+        final Game game = new GameBuilder().player1(p1).player2(p2).field(field).name("XO").build();
+        final ConsoleView cv = new ConsoleView();
 
-    private static void checkField(String winnerStr, String... field) {
-        final Figure winner = winnerStr != null ? Figure.valueOf(winnerStr) : null;
-        final Field f = new Field();
-        for (int i = 0; i < field.length; i++) {
+        final StringBuilder separatorBuilder = new StringBuilder();
+        final String expectedSeparator = "~~~~~~~~~~~\n";
+        cv.generateSeparator(separatorBuilder);
+        final String actualSeparator = separatorBuilder.toString();
 
-            final int x = i % 3;
-
-            final int y = (i - i % 3) / 3;
-            final Point p = new Point(x, y);
-            final Figure figure = field[i] != null ? Figure.valueOf(field[i]) : null;
-            f.setFigure(p, figure);
-
+        if (!expectedSeparator.equals(actualSeparator)) {
+            throw new RuntimeException(String.format("Separator generated: \"%s\" instead of \"%s\"", actualSeparator, expectedSeparator));
         }
-        final WinnerController wc = new WinnerController();
-        if (winner == null && wc.getWinner(f) != null)
-            throw new RuntimeException(String.format("Incorrectly show winner, it shows: %s", wc.getWinner(f)));
-        if (winner != null && !winner.equals(wc.getWinner(f)))
-            throw new RuntimeException(String.format("Incorrectly show winner, it shows: %s", wc.getWinner(f)));
 
-    }
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(1, 0), Figure.O);
 
-    private static void testPrivateMethod(final String methodName, final Class zClass, final Class<?>... parameterTypes) {
-        try {
-            final Method field = zClass.getDeclaredMethod(methodName, parameterTypes);
-            if (!Modifier.isPrivate(field.getModifiers())) throw new RuntimeException("method " + methodName + " has incorrect visibility level");
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-            throw new RuntimeException("method " + methodName + " not found");
+        final StringBuilder lineBuilder = new StringBuilder();
+        final String expectedLine = " X | O |   \n";
+        cv.generateLine(field, 0, lineBuilder);
+        final String actualLine = lineBuilder.toString();
+
+        if (!expectedLine.equals(actualLine)) {
+            throw new RuntimeException(String.format("Line generated: \"%s\" instead of \"%s\"", actualLine, expectedLine));
         }
-    }
 
-    private static void testPublicMethod(final String methodName, final Class zClass, final Class<?>... parameterTypes) {
-        try {
-            final Method field = zClass.getDeclaredMethod(methodName, parameterTypes);
-            if (!Modifier.isPublic(field.getModifiers())) throw new RuntimeException("method " + methodName + " has incorrect visibility level");
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-            throw new RuntimeException("method " + methodName + " not found");
-        }
+        cv.show(game);
     }
 
 }
--- src/io/hexlet/xo/view/ConsoleView.java
+++ src/io/hexlet/xo/view/ConsoleView.java
@@ -1,13 +1,49 @@
 package io.hexlet.xo.view;
 
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
 import io.hexlet.xo.model.Game;
-
+import io.hexlet.xo.model.Point;
 
 public class ConsoleView {
 
-    public void show(final Game game) {}
+    public void show(final Game game) {
+        final Field field = game.getField();
+        final StringBuilder fieldBuilder = new StringBuilder();
+        for (int x = 0; x < field.getSize(); x++) {
+            if (x != 0) {
+                generateSeparator(fieldBuilder);
+            }
+            generateLine(field, x, fieldBuilder);
+        }
+    }
 
     public boolean move(final Game game) {
-        return false;
+        return true;
     }
+
+    public void generateLine(final Field field, final int x, final StringBuilder sb) {
+        for (int i = 0; i < 3; i++) {
+            sb.append(" ");
+            Figure fig = field.getFigure(new Point(i, x));
+            if(fig != null){
+                sb.append(fig.toString());
+            }
+            else{
+                sb.append(" ");
+            }
+            sb.append(" ");
+            if(i != 2){
+                sb.append("|");
+            }
+            else{
+                sb.append("\n");
+            }
+        }
+    }
+
+    public void generateSeparator(final StringBuilder sb) {
+        sb.append("~~~~~~~~~~~\n");
+    }
+
 }
